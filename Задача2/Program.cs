df65256798311166e6bbde384bf99449e1d5bd21
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-22 21:35:58 +0700

    На вход люб число, на вых 3-я цифра

---
+++ Задача2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int tmp = num;
+int digs = (int)Math.Log10(num) + 1;
+if (digs < 3) Console.WriteLine("третьей цифры нет");
+else
+{
+    tmp = num / (int)Math.Pow(10, digs - 3);
+    num = tmp % 10;
+    Console.WriteLine(num);
+}
