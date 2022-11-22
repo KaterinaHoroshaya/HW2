3ce2c566fed82f5f7015ef0d9de3c542cad879dd
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-22 21:35:42 +0700

    На вход 3-зн, на вых - 2 цифра

---
+++ Задача1/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/* 456 -> 5
+782 -> 8
+918 -> 1 */
+
+int randomNumber = new Random().Next(100,1000);
+int firstDigit = randomNumber / 10; //45
+int secondDigit = firstDigit % 10; //5
+
+Console.WriteLine(randomNumber);
+
+Console.WriteLine($"{secondDigit}");
